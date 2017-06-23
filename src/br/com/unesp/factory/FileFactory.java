865141fd01a1f571bd12f580480c93e76e2d7b23
Yuji Negoro <yuji.negoro@gmail.com>
2017-06-22 21:22:08 -0300

    Add files via upload

--- src/br/com/unesp/factory/FileFactory.java
+++ src/br/com/unesp/factory/FileFactory.java
@@ -3,6 +3,11 @@ package br.com.unesp.factory;
 import br.com.unesp.beans.Arquivo;
 import br.com.unesp.beans.Bib;
 
+/**
+ * 
+ *  Fornece as instancias da classe Bib  
+ *
+ */
 public class FileFactory {
 
 	public static Arquivo getInstance(){
