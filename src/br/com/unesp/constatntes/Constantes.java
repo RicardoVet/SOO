3d9faed10f7247cf295c50d7c495ae57c9fb5048
Yuji Negoro <yuji.negoro@gmail.com>
2017-06-22 21:21:45 -0300

    Add files via upload

--- src/br/com/unesp/constatntes/Constantes.java
+++ src/br/com/unesp/constatntes/Constantes.java
@@ -1,5 +1,12 @@
 package br.com.unesp.constatntes;
 
+
+/**
+ * 
+ * Constantes que serão utilzadas na geração do bib.tex padronizado
+ * 
+ *
+ */
 public class Constantes {
 	
 	public final static String novaLinha = "<br />";
